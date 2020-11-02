aa5c3748bc5e7ca714dfd8b87c29815cc47fb283
kobayashiukyo <u.k.knua0419@gmail.com>
2020-11-02 23:52:16 +0900

    first_commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+{
+  const timer = document.getElementById('timer');
+  const start = document.getElementById('start');
+  const stop = document.getElementById('stop');
+  const reset = document.getElementById('reset');
+
+  let startTime;
+  let timeoutId;
+
+  function countUp() {
+    const d = new Date(Date.now() - startTime);
+    const m = String(d.getMinutes()).padStart(2, '0');
+    const s = String(d.getSeconds()).padStart(2, '0');
+    const ms = String(d.getMilliseconds()).padStart(3, '0');
+    timer.textContent = `${m}:${s}.${ms}`;
+
+    timeoutId = setTimeout(() => {
+      countUp();
+    }, 10);
+  }
+
+  start.addEventListener('click', () => {
+    startTime = Date.now();
+    countUp();
+  });
+
+  stop.addEventListener('click', () => {
+    clearTimeout(timeoutId);
+  });
+
+  reset.addEventListener('click', () => {
+    timer.textContent = '00:00.000';
+  });
+}
